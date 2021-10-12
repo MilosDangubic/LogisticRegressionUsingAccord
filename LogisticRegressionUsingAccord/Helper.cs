97a00ac3e1400df87b70d779e97830cbc7d413c0
MilosDangubic <>
2021-10-12 17:11:12 +0200

    Initial commit

---
+++ LogisticRegressionUsingAccord/Helper.cs
@@ -0,0 +1,195 @@
+﻿using Accord.Statistics.Models.Regression;
+using Accord.Statistics.Models.Regression.Fitting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogisticRegressionUsingAccord
+{
+    public class Helper
+    {
+
+
+        public static List<string> extractFeatureNames(string file, char sep)
+        {
+            List<string> featureNames = new List<string>();
+            System.IO.FileStream ifs = new System.IO.FileStream(file, System.IO.FileMode.Open);
+            System.IO.StreamReader sr = new System.IO.StreamReader(ifs);
+            string line = sr.ReadLine();
+            string[] lineParts = line.Split(sep);
+
+            for (int i = 0; i < lineParts.Length - 1; i++)
+            {
+                featureNames.Add(lineParts[i]);
+            }
+
+            return featureNames;
+
+        }
+        public static void randomize(double[][] data)
+        {
+            int n;
+            n = data.Length;
+            Random r = new Random();
+            for (int i = n - 1; i > 0; i--)
+            {
+                int j = r.Next(0, i + 1);
+                for (int k = 0; k < data[i].Length; k++)
+                {
+                    double temp = data[i][k];
+                    data[i][k] = data[j][k];
+                    data[j][k] = temp;
+                }
+
+
+            }
+        }
+
+        private static double[][] MatrixCreate(int rows, int cols)
+        {
+            double[][] result = new double[rows][];
+            for (int i = 0; i < rows; ++i)
+                result[i] = new double[cols];
+            return result;
+        }
+
+        private static bool[][] MatrixCreateB(int rows, int cols)
+        {
+            bool[][] result = new bool[rows][];
+            for (int i = 0; i < rows; ++i)
+                result[i] = new bool[cols];
+            return result;
+        }
+        public static double[][] RemoveLabels(double[][] datawithLabel)
+        {
+            double[][] data = new double[datawithLabel.Length][];
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = new double[datawithLabel[i].Length - 1];
+            }
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                for (int j = 0; j < data[i].Length; j++)
+                {
+                    data[i][j] = datawithLabel[i][j];
+                }
+            }
+            return data;
+        }
+        public static double[] ExtractLabels(double[][] data)
+        {
+            double[] labels = new double[data.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                labels[i] = data[i][data[i].Length - 1];
+            }
+            return labels;
+
+        }
+        public static double[][] MatrixLoad(string file, bool header, char sep)
+        {
+            string line = "";
+            string[] tokens = null;
+            int ct = 0;
+            int rows, cols;
+            System.IO.FileStream ifs =
+              new System.IO.FileStream(file, System.IO.FileMode.Open);
+            System.IO.StreamReader sr =
+              new System.IO.StreamReader(ifs);
+            while ((line = sr.ReadLine()) != null)
+            {
+                ++ct;
+                tokens = line.Split(sep);
+            }
+            sr.Close(); ifs.Close();
+            if (header == true)
+                rows = ct - 1;
+            else
+                rows = ct;
+            cols = tokens.Length;
+            double[][] result = MatrixCreate(rows, cols);
+
+          
+            int i = 0; // row index
+            ifs = new System.IO.FileStream(file, System.IO.FileMode.Open);
+            sr = new System.IO.StreamReader(ifs);
+
+            if (header == true)
+                line = sr.ReadLine();  // consume header
+            while ((line = sr.ReadLine()) != null)
+            {
+                tokens = line.Split(sep);
+                for (int j = 0; j < cols; ++j)
+                    result[i][j] = double.Parse(tokens[j], System.Globalization.CultureInfo.InvariantCulture);
+                ++i; // next row
+            }
+            sr.Close(); ifs.Close();
+            return result;
+        }
+   
+
+        public static double CalculateAccuracy(int[] predicted, int[] actual)
+        {
+            double acc = 0;
+            for (int i = 0; i < predicted.Length; i++)
+            {
+                if (predicted[i] == actual[i])
+                {
+                    acc++;
+                }
+            }
+            acc = acc / predicted.Length;
+            return acc;
+        }
+
+        public static int[] ConvertDoubleToInt(double[] array) 
+        {
+            int[] newArray = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = (int)array[i];
+            }
+            return newArray;
+        }
+
+        public static void SplitTrainTestData(double[][] data, double[] allLabels, double[][] trainData, double[][] testData, double[] trainLabels, double[] testLabels, int n)
+        {
+            int nTrain = n / 100 * 70;
+            for (int i = 0; i < nTrain; i++)
+            {
+                trainLabels[i] = allLabels[i];
+                trainData[i] = new double[data[i].Length];
+                for (int j = 0; j < data[i].Length; j++)
+                {
+                    trainData[i][j] = data[i][j];
+                }
+            }
+            for (int i = 0; i < n - nTrain; i++)
+            {
+                testLabels[i] = allLabels[i + nTrain];
+                testData[i] = new double[data[i].Length];
+                for (int j = 0; j < data[i].Length; j++)
+                {
+                    testData[i][j] = data[i + nTrain][j];
+                }
+            }
+
+        }
+
+        public static double EvaluateModel(double[][] X, int [] Y)
+        {
+
+            MultinomialLogisticLearning<Accord.Math.Optimization.GradientDescent> lrn = new MultinomialLogisticLearning<Accord.Math.Optimization.GradientDescent>();
+            MultinomialLogisticRegression mlr = lrn.Learn(X, Y);
+            double acc = 0;
+            int[] decisions = mlr.Decide(X);
+            acc = CalculateAccuracy(decisions, Y);
+            return acc;
+
+        }
+
+
+
+    }
+}
---
+++ LogisticRegressionUsingAccord/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Accord.MachineLearning.VectorMachines.Learning;
+using Accord.Statistics.Kernels;
+using Accord.Statistics.Models.Regression;
+using Accord.Statistics.Models.Regression.Fitting;
+namespace LogisticRegressionUsingAccord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("======LOGISTIC REGRESSION USING ACCORD ============");
+            string fileName = "../../../winequality-red.txt";
+            //Ucitavanje podataka
+            Console.WriteLine("Loading Data...");
+            double[][] data = Helper.MatrixLoad(fileName, true, ';');
+			//Fisher-Yates algoritam za permutaciju observacija u skupu podataka. Menjamo raspored observacija u skupu
+            //Helper.randomize(data);
+            double[] allLabels = Helper.ExtractLabels(data);
+            data = Helper.RemoveLabels(data);
+            int[] allLabelsI = Helper.ConvertDoubleToInt(allLabels);
+            Console.WriteLine("Feature selection");
+            data=FeatureSelection.selectFeatures(data, allLabelsI,3);
+
+			//70% podataka koristimo za trening skup,30% podataka za test skup
+            int n = data.Length;
+            int nTrain = n / 100 * 70;
+            double[][] trainData = new double[nTrain][];
+            double[] trainLabels = new double[nTrain];
+            double[][] testData = new double[n - nTrain][];
+            double[] testLabels = new double[n-nTrain];
+            Helper.SplitTrainTestData(data, allLabels, trainData, testData, trainLabels, testLabels, n);
+
+            //iz trening i test skupa izvlacimo poslednju kolonu jer se u njoj nalaze labele(klase) kojima observacije pripadaju
+
+            int[] trainLabelsI = Helper.ConvertDoubleToInt(trainLabels);
+            int[] testLabelsI = Helper.ConvertDoubleToInt(testLabels);
+
+            //Kreiranje objekta klase MultinomialLogisticLearning pomocu kojeg cemo uciti model
+            MultinomialLogisticLearning<Accord.Math.Optimization.GradientDescent> lrn = new MultinomialLogisticLearning<Accord.Math.Optimization.GradientDescent>();
+		
+			//Metoda Learn vraca istrenirani model 
+            MultinomialLogisticRegression mlr = lrn.Learn(trainData, trainLabelsI);
+			//Nakon treniranja vrsimo predikciju nad trening skupom i merimo preciznost
+            int[] decisions = mlr.Decide(trainData);
+            double trainAcc = Helper.CalculateAccuracy(decisions, trainLabelsI);
+            int[] testDecisions = mlr.Decide(testData);
+
+            double testAcc = Helper.CalculateAccuracy(testDecisions,testLabelsI);
+            Console.WriteLine("Preciznost na trening skupu je  {0}", trainAcc * 100);
+			//Vrsimo predikciju nad test skupom i merimo preciznost
+            Console.WriteLine("Preciznost na test skupu je  {0}", testAcc * 100);
+
+
+
+
+
+        }
+    }
+}
